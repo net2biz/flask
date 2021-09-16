118639b91060b966eb3825cf6fa7e71ac0448896
sanjay <net2biz@gmail.com>
2021-09-16 12:04:56 +1000

    made changes

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from markupsafe import escape
+
 app= Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World</p>"
 
-    
+@app.route("/process")
+def hello_process():
+    return "<p>Hello Process</p>"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
